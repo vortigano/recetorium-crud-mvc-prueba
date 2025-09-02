94053998a9d8d5081f8433b465ac08c10115364f
Charly <carlosecimino@gmail.com>
2025-09-02 14:23:55 -0300

    Inicio del proyecto

---
+++ src/main/java/ar/charlycimino/recetorium/crud/mvc/prueba/RecetoriumCrudMvcPrueba.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ar.charlycimino.recetorium.crud.mvc.prueba;
+
+/**
+ *
+ * @author Charly Cimino
+ */
+public class RecetoriumCrudMvcPrueba {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
